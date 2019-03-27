4643ffaf9875b19ff5578ab08af44ba0279ad7fc
alyaazab <alya.113@live.com>
2019-03-28 00:23:23 +0200

    Swapping completed with colour changes.

--- src/sample/GraphicalSort.java
+++ src/sample/GraphicalSort.java
@@ -1,87 +1,56 @@
 package sample;
 
-import javafx.animation.PathTransition;
-import javafx.animation.TranslateTransition;
-import javafx.geometry.Point2D;
+import javafx.animation.*;
 import javafx.scene.Node;
-import javafx.scene.control.Control;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
-import org.w3c.dom.css.Rect;
-
-import java.awt.*;
 
 
 public class GraphicalSort {
 
+    SequentialTransition sequentialTransition = new SequentialTransition();
+
+    Rectangle rect1 = null;
+    Rectangle rect2 = null;
+    int index1 = 0, index2=0;
+
     Pane pane;
-    private void swap(int i, int j, int[] array) {
-
-        System.out.println("swapping:");
-        System.out.println("i = " + i);
-        System.out.println("j = " + j);
-
-        Rectangle rect1 = null;
-        Rectangle rect2 = null;
-        int index1 = 0, index2=0;
 
 
+    private int getCurrentRectangle(int index){
         for (int k = 0; k < pane.getChildren().size(); k++) {
             Node node = pane.getChildren().get(k);
-
+            System.out.println("this node is a " + node.getId());
             if(node instanceof Rectangle) {
-                if(Integer.parseInt(node.getId())==i)
+                if(Integer.parseInt(node.getId())==index)
                 {
-                    rect1 = (Rectangle) node;
-                    index1 = k;
-
-                    System.out.println("rect 1 id = " + rect1.getId() + "    node id: " + node.getId());
-                    System.out.println("k = " + k);
-                }
-                if(Integer.parseInt(node.getId())==j)
-                {
-                    rect2 = (Rectangle) node;
-                    index2=k;
-                    System.out.println("rect 1 id = " + rect2.getId() + "    node id: " + node.getId());
-                    System.out.println("k = " + k);
+                    return k;
                 }
             }
         }
+        return 0;
+    }
 
-//        for(Node node: pane.getChildren())
-//        {
-//            if(node instanceof Rectangle) {
-//                if(Integer.parseInt(node.getId())==i)
-//                {
-//                    System.out.println("move this rect");
-//                    rect1 = (Rectangle) node;
-//                    node.setId(j+"");
-//                }
-//                if(Integer.parseInt(node.getId())==j)
-//                {
-//                    rect2 = (Rectangle) node;
-//                    node.setId(i+"");
-//
-//                }
-//            }
-//        }
+    private void swap(int i, int j, int[] array) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
 
-        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(1), rect1);
-        translateTransition.setByX(40);
-        translateTransition.setCycleCount(1);
-        translateTransition.setAutoReverse(true);
+        index1 = getCurrentRectangle(i);
+        index2 = getCurrentRectangle(j);
 
-        translateTransition.play();
+        rect1 = (Rectangle) pane.getChildren().get(index1);
+        rect2 = (Rectangle) pane.getChildren().get(index2);
 
-        TranslateTransition translateTransition2 = new TranslateTransition(Duration.seconds(1), rect2);
-        translateTransition2.setByX(-40);
-        translateTransition2.setCycleCount(1);
-        translateTransition2.setAutoReverse(true);
 
-        translateTransition2.play();
+        TranslateTransition tt1 = addPositiveTranslateTransition(rect1);
+        TranslateTransition tt2 = addNegativeTranslateTransition(rect2);
+        FillTransition ft1 = addFillTransition(rect1);
+        FillTransition ft2 = addFillTransition(rect2);
+        FillTransition rft1 = removeFillTransition(rect1);
+        FillTransition rft2 = removeFillTransition(rect2);
 
         pane.getChildren().get(index1).setId(j+"");
         pane.getChildren().get(index2).setId(i+"");
@@ -90,53 +59,113 @@ public class GraphicalSort {
         pane.getChildren().remove(index1);
         pane.getChildren().add(index2, temp1);
 
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        sequentialTransition.getChildren().add(new ParallelTransition(ft1, ft2));
+        sequentialTransition.getChildren().add(new ParallelTransition(tt1, tt2));
+        sequentialTransition.getChildren().add(new ParallelTransition(rft1, rft2));
+        sequentialTransition.getChildren().add(new PauseTransition(Duration.seconds(0.5)));
     }
 
-    //Bubble Sort
-    //Time Complexity = O(n^2)
-    //Best Case = O(n) ---> when array is already sorted
-    //Worst Case occurs when array is reverse sorted
-    //Sorted in place
-    //End of array is always sorted ("i" elements at the end of the array)
+
+    private TranslateTransition addPositiveTranslateTransition(Rectangle rectangle){
+
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), rectangle);
+        translateTransition.setByX(40);
+        translateTransition.setCycleCount(1);
+        translateTransition.setAutoReverse(true);
+
+        return translateTransition;
+
+    }
+
+    private TranslateTransition addNegativeTranslateTransition(Rectangle rectangle){
+
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), rectangle);
+        translateTransition.setByX(-40);
+        translateTransition.setCycleCount(1);
+        translateTransition.setAutoReverse(true);
+
+        return translateTransition;
+
+    }
+
+    private FillTransition addFillTransition(Rectangle rectangle){
+
+        FillTransition fillTransition = new FillTransition();
+        fillTransition.setShape(rectangle);
+        fillTransition.setDuration(new Duration(500));
+        fillTransition.setToValue(Color.GOLD);
+        fillTransition.setCycleCount(1);
+        fillTransition.setAutoReverse(true);
+
+        return fillTransition;
+
+    }
+
+
+    private FillTransition removeFillTransition(Rectangle rectangle){
+
+        FillTransition removeFillTransition = new FillTransition();
+        removeFillTransition.setShape(rectangle);
+        removeFillTransition.setDuration(new Duration(500));
+        removeFillTransition.setToValue(Color.BLUE);
+        removeFillTransition.setCycleCount(1);
+        removeFillTransition.setAutoReverse(true);
+
+        return removeFillTransition;
+
+    }
+
+
+
+
     public int[] bubbleSort(int[] array, Pane pane){
         printArray(array);
-
+        int indexA = 0, indexB=0;
+        Rectangle rectangleA, rectangleB;
+        FillTransition fillTransitionA, fillTransitionB, removeFillTransitionA, removeFillTransitionB;
         this.pane = pane;
-        int temp;
         int sorted;
-        int numOfSwaps=0;
 
-        //length-1 because the array is already sorted when we get to the last pass
         for(int i=0; i<array.length-1; i++)
         {
             sorted = 1;
-
-            //length-1 to avoid array index out of bounds exception
-            //length-1-i because the last "i" elements are sorted
             for(int j=0; j<array.length-i-1; j++)
             {
-                //if any 2 consecutive elements are in the wrong order, swap them
                 if(array[j] > array[j+1])
                 {
                     sorted = 0;
                     swap(j, j+1, array);
-                    numOfSwaps++;
-                    if(numOfSwaps==2)
-                        break;
                 }
+                else
+                {
+                    indexA = getCurrentRectangle(j);
+                    indexB = getCurrentRectangle(j+1);
+                    rectangleA = (Rectangle) pane.getChildren().get(indexA);
+                    fillTransitionA = addFillTransition(rectangleA);
+                    rectangleB = (Rectangle) pane.getChildren().get(indexB);
+                    fillTransitionB = addFillTransition(rectangleB);
+                    sequentialTransition.getChildren().add(new ParallelTransition(fillTransitionA, fillTransitionB));
+                    sequentialTransition.getChildren().add(new PauseTransition(Duration.seconds(0.5)));
+                    removeFillTransitionA = removeFillTransition(rectangleA);
+                    removeFillTransitionB = removeFillTransition(rectangleB);
+
+                    sequentialTransition.getChildren().add(new ParallelTransition(removeFillTransitionA, removeFillTransitionB));
+
+                }
+
             }
-            if(numOfSwaps==2)
-                break;
+
             if(sorted==1)
                 break;
 
             printArray(array);
         }
+
+        System.out.println("seq size = " + sequentialTransition.getChildren().size());
+        sequentialTransition.play();
         return array;
     }
+
 
     public void printArray(int[] array){
 
